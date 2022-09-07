d18be842259576fb25388c3ce6d1854dbfd1e3a3
LunaDesarrollosW <104751564+LunaDesarrollosW@users.noreply.github.com>
2022-09-07 16:23:54 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<h1>PRACTICO1</h1>
+
+<?php
+$saludo1 = "hola mundo";
+echo $saludo1;
+?>
+
+<?PHP
+$datoA = "hola";
+$datoB = "mundo";
+echo $datoA + $datoB ;
+ ?>
+
+ <?php
+ $valor1 = 100 ;
+ $valor2 = 30 ;
+ echo $valor1 + $valor2 ;
+  ?>
+
+  <?php
+  $valor1 = 100 ;
+  $valor2 = 30 ;
+  echo $valor1 - $valor2 ;
+  ?>
+
+  <?php
+  $valor1 = 100;
+  $valor2 = 30;
+   echo $valor1 * $valor2 ;
+    ?>
+
+  <?php
+  $valor1 = 100;
+  $valor2 = 30;
+  echo $valor1 / $valor2 ;
+  ?>
+
+  <?php
+  $gradosC = 20;
+  $gradosF = c +1,8 +32;
+  echo $gradosF;
+  ?>
+
+  <?php
+  $base = 18;
+  $altura = 12;
+  $perimetro = $base*2 + $altura*2;
+  $superficie = $base * $altura;
+   ?>
+
+
+  </body>
+</html>
